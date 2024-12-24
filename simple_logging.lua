c97bffff61bfe9ca2c2d6647a56b715fdc0e2d6f
flin16 <flin16@illinois.edu>
2024-12-24 17:36:05 -0600

    Auto commit

--- simple_logging.lua
+++ simple_logging.lua
@@ -1,16 +1,42 @@
+function locate(key)
+	for i = 1, 16 do
+		turtle.select(i)
+		local raw = turtle.getItemDetail()
+		if raw then
+			local name = raw.name
+			if string.find(name, key) then
+				return i
+			end
+		end
+	end
+end
+
+function refuel()
+	if locate("log") then
+		turtle.refuel()
+	end
+end
+
+function plant()
+	if locate("sapling") then
+		turtle.place()
+	end
+end
+
 while true do
-	turtle.refuel()
-	_G.sleep(600)
+	refuel()
 	while true do
-		_, a = turtle.inspect()
-		if a.tags["minecraft:logs"] == true then
+		local exi, a = turtle.inspect()
+		if exi and a.tags["minecraft:logs"] == true then
 			turtle.dig()
+			turtle.digUp()
 			turtle.up()
 		else
 			break
 		end
 	end
-
 	while turtle.down() do
 	end
+	plant()
+	_G.sleep(1)
 end
