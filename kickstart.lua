7ed4a17d693a1091816751b62e1c7ee7b647866f
flin16 <flin16@illinois.edu>
2024-12-24 17:00:23 -0600

    Auto commit

--- kickstart.lua
+++ kickstart.lua
@@ -1,6 +1,6 @@
--- Run wget "https://github.com/flin16/miner/blob/main/kickstart.lua" to download this file
+-- Run wget "https://raw.githubusercontent.com/flin16/miner/refs/heads/main/kickstart.lua" to download this file
 file_list = { "kickstart", "simple_logging" }
 
 for _, file in ipairs(file_list) do
-	http.get("https://github.com/flin16/miner/blob/main/" + file + ".lua", file + ".lua")
+	http.get("https://raw.githubusercontent.com/flin16/miner/refs/heads/main/" + file + ".lua", file + ".lua")
 end
